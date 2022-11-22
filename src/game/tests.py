b03fa52cf9ebf0270437b7ee05b45b6bcf62db20
Bennie <bennie@comparisure.co.za>
2022-11-22 21:37:28 +0200

    add lobby creation test

--- src/game/tests.py
+++ src/game/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from game.models import Lobby
+
+
+class ViewTests(TestCase):
+
+    def test_lobby_create(self):
+        # Setup
+        data = {
+            'name': 'lobby-name'
+        }
+        n_lobbies = Lobby.objects.count()
+        # Execute
+        response = self.client.post('/api/lobbies/', data=data)
+        # Assert
+        self.assertEquals(response.status_code, 201)
+        self.assertEquals(Lobby.objects.count(), n_lobbies + 1)
