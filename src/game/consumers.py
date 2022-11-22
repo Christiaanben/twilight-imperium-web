ffd9ea2866fed2e3c3b9d0594eca8bbb16061438
Bennie <bennie@comparisure.co.za>
2022-11-23 00:25:08 +0200

    replace room_name with lobby_id

--- src/game/consumers.py
+++ src/game/consumers.py
@@ -5,8 +5,8 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = "chat_%s" % self.room_name
+        lobby_id = self.scope["url_route"]["kwargs"]["lobby_id"]
+        self.room_group_name = "chat_%s" % lobby_id
 
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
