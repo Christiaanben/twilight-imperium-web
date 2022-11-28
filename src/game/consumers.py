fdbb6f092436223b196870b8ae60a4c4dbccd340
Bennie <bennie@comparisure.co.za>
2022-11-29 01:04:01 +0200

    added handle_new_or_returning_player skeleton function

--- src/game/consumers.py
+++ src/game/consumers.py
@@ -5,18 +5,27 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        lobby_id = self.scope["url_route"]["kwargs"]["lobby_id"]
-        self.room_group_name = "chat_%s" % lobby_id
+        lobby_id = self.scope['url_route']['kwargs']['lobby_id']
+        self.room_group_name = 'chat_%s' % lobby_id
 
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         await self.accept()
+        print('self.user', self.scope['user'])
 
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": 'new user'}
+            self.room_group_name, {'type': 'handle_new_or_returning_player', 'user': 'John'}
         )
+
+    async def handle_new_or_returning_player(self, data):
+        """
+        Checks if the user is signed in. Then, if they are already a part of the lobby do nothing (or update presence).
+        Else add them to the lobby and let everyone in the lobby know of the new player
+        """
+        print('handling new or returning user', data)
+        await self.send(text_data=json.dumps({'new_player': 'John'}))
 
     async def disconnect(self, close_code):
         # Leave room group
@@ -25,16 +34,16 @@ class GameConsumer(AsyncWebsocketConsumer):
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        message = text_data_json['message']
 
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": message}
+            self.room_group_name, {'type': 'chat_message', 'message': message}
         )
 
     # Receive message from room group
     async def chat_message(self, event):
-        message = event["message"]
+        message = event['message']
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({'message': message}))
