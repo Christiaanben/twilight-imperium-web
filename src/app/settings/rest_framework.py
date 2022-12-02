195a01646f2140b84c9fe944adb5b7d02d7bb747
Bennie <bennie@comparisure.co.za>
2022-12-03 00:57:39 +0200

    fix tests

--- src/app/settings/rest_framework.py
+++ src/app/settings/rest_framework.py
@@ -2,7 +2,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
