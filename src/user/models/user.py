c75e6418fa9cd8f9ed5cd54d14bb585538c3eef0
Bennie <bennie@comparisure.co.za>
2023-01-10 01:38:15 +0200

    revert `users` app to `user`

--- src/user/models/user.py
+++ src/user/models/user.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
 class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
     display_name = models.CharField(max_length=30)
+
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
