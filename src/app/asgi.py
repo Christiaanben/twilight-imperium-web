9c5df769a7863cd5792e293e3bdbf4068aece934
Bennie <bennie@comparisure.co.za>
2023-01-10 02:47:01 +0200

    fix server startup

--- src/app/asgi.py
+++ src/app/asgi.py
@@ -14,13 +14,14 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+asgi_app = get_asgi_application()
+
 import game.routing
 from app.utils.middleware import TokenAuthMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": asgi_app,
     'websocket': TokenAuthMiddleware(
         AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(game.routing.websocket_urlpatterns))
