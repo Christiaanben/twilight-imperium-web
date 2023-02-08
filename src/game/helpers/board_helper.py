acc4060a81874ae0c59bba8452c68fc9bc79c568
Bennie <bennie@comparisure.co.za>
2023-02-08 23:00:07 +0200

    add 2 player board setup with seeds

--- src/game/helpers/board_helper.py
+++ src/game/helpers/board_helper.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from app.settings import logger
 from game.models import Game, System, BasePlanet, Planet, BaseSystem
 
 DEFAULT_BOARDS = {
@@ -13,10 +14,32 @@ DEFAULT_BOARDS = {
         # CENTER
         {'base_id': 18, 'q': 0, 'r': 0},
         # RING 1
-        {'base_id': None, 'q': 1, 'r': 0},
-        {'base_id': 42, 'q': 0, 'r': 1},
-        {'base_id': None, 'q': -1, 'r': 0},
-        {'base_id': 44, 'q': 0, 'r': -1},
+        {'base_id': 23, 'q': 1, 'r': 0},
+        {'base_id': 50, 'q': 0, 'r': 1},
+        {'base_id': 37, 'q': -1, 'r': 1},
+        {'base_id': 32, 'q': -1, 'r': 0},
+        {'base_id': 49, 'q': 0, 'r': -1},
+        {'base_id': 26, 'q': 1, 'r': -1},
+        # RING 2
+        # No 2,0
+        {'base_id': 48, 'q': 1, 'r': 1},
+        {'base_id': 21, 'q': 0, 'r': 2},
+        {'base_id': 29, 'q': -1, 'r': 2},
+        {'base_id': 38, 'q': -2, 'r': 2},
+        {'base_id': 19, 'q': -2, 'r': 1},
+        # No -2,0
+        {'base_id': 39, 'q': -1, 'r': -1},
+        {'base_id': 42, 'q': 0, 'r': -2},
+        {'base_id': 46, 'q': 1, 'r': -2},
+        {'base_id': 35, 'q': 2, 'r': -2},
+        {'base_id': 28, 'q': 2, 'r': -1},
+        # # RING 3
+        {'base_id': 41, 'q': -2, 'r': 3},
+        {'base_id': None, 'q': -3, 'r': 3},  # Faction 1
+        {'base_id': 30, 'q': -3, 'r': 2},
+        {'base_id': 36, 'q': 2, 'r': -3},
+        {'base_id': None, 'q': 3, 'r': -3},  # Faction 2
+        {'base_id': 24, 'q': 3, 'r': -2},
     ],
     3: [
         # CENTER
@@ -73,6 +96,7 @@ def generate_board(game: Game):
     systems = [System(game=game, **system) for system in DEFAULT_BOARDS[n_players]]
     for player, system in zip(game.players.all(), filter(lambda s: s.base_id is None, systems)):
         system.base = BaseSystem.objects.get(faction=player.faction)
+    logger.info(f"Creating {len(systems)} systems")
     System.objects.bulk_create(systems)
     base_planets = BasePlanet.objects.filter(base_system_id__in=[system.base_id for system in systems])
     planets = [_init_planet(systems, base_planet) for base_planet in base_planets]
