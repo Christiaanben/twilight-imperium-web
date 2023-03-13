e54ef5e9b8ed6d8bf36c613b3db7a9e284f8b594
Bennie <bennie@comparisure.co.za>
2023-03-14 00:11:48 +0200

    Add units to GameSerializer

--- src/game/serializers/game_serializer.py
+++ src/game/serializers/game_serializer.py
@@ -1,16 +1,22 @@
 from rest_framework import serializers
 
-from game.models import Game
-from . import PlayerSerializer
+from game.models import Game, Unit
 from .strategy_serializer import StrategySerializer
 from .system_serializer import SystemSerializer
+from .unit_serializer import UnitSerializer
+from .player_serializer import PlayerSerializer
 
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
-        fields = ['systems', 'phase', 'strategies', 'players']
+        fields = ['systems', 'phase', 'strategies', 'players', 'units']
 
     systems = SystemSerializer(many=True)
     strategies = StrategySerializer(many=True)
     players = PlayerSerializer(many=True)
+    units = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_units(game: Game):
+        return UnitSerializer(Unit.objects.filter(owned_by__game=game), many=True).data
