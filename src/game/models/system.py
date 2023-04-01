54ab9fabc5ae7e3215dba603af9f3694b93acba7
Bennie <bennie@comparisure.co.za>
2023-04-01 22:32:52 +0200

    add 'activated_by' to System

--- src/game/models/system.py
+++ src/game/models/system.py
@@ -5,6 +5,7 @@ class System(models.Model):
     # Foreign keys
     game = models.ForeignKey('game.Game', on_delete=models.CASCADE)
     base = models.ForeignKey('game.BaseSystem', on_delete=models.CASCADE)
+    activated_by = models.ManyToManyField('game.Player', related_name='activated_systems', blank=True)
     # Fields
     q = models.SmallIntegerField()
     r = models.SmallIntegerField()
@@ -16,4 +17,7 @@ class System(models.Model):
         unique_together = [('game', 'q', 'r'), ('game', 'base')]
 
     def __repr__(self):
+        return f'System(id={self.id}, base={self.base_id}, game={self.game_id})'
+    
+    def __str__(self):
         return f'{self.game_id}-{self.base_id}'
