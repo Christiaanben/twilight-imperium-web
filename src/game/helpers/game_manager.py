738f3c651d12a6a3c1b529ed12fd40120b93e540
ChristiaanL <christiaanl.lamprecht1@gmail.com>
2023-05-01 22:42:08 +0200

    Added new card fixtures and card objective select at start.

--- src/game/helpers/game_manager.py
+++ src/game/helpers/game_manager.py
@@ -1,7 +1,6 @@
 from django.utils import timezone
-
 from app.settings import logger
-from game.models import Game, Strategy, BaseStrategy, BaseUnit, Unit, System
+from game.models import Game, Strategy, BaseStrategy, BaseUnit, Unit, System, BaseCard, Card
 
 
 def start_game(game_id: str):
@@ -12,6 +11,7 @@ def start_game(game_id: str):
     game.save()
     _create_strategies(game)
     _create_units(game)
+    _create_starting_objectives(game)
     # board_helper.generate_board(game)
     # GameConsumer.create_new_game(game_id)
 
@@ -34,3 +34,16 @@ def _create_units(game: Game):
             base = BaseUnit.objects.get(type=unit_type)
             units.append(Unit(base=base, owned_by=player, system=home_system))
         Unit.objects.bulk_create(units)
+
+
+def _create_starting_objectives(game: Game):
+    """
+    Create first 2 starting objectives.
+    :param game: Game
+    :return:
+    """
+    picked_objectives = []
+    for base_objective in BaseCard.objects.filter(type='stage_1').order_by("?")[:2]:
+        picked_objectives.append(Card(base=base_objective, game=game))
+    Card.objects.bulk_create(picked_objectives)
+
